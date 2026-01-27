4009794d48835f8e9297f3a3d01652e225b5b5c5
Satria BayuNagara <nagarasatria104@gmail.com>
2026-01-27 17:50:51 +0700

    perbaikan ocr

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.e_logbook"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
      ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -31,10 +31,16 @@ android {
     }
 
     buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            isShrinkResources = false
+    }
+
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 }
