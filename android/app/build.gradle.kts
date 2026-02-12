af1ce907820eaef50b8bffe24f2a184fcae46558
Akbar Maulana <akbar.maulana000501@gmail.com>
2026-02-12 10:20:02 +0700

    push

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,6 +17,7 @@ android {
 
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
+        freeCompilerArgs += listOf("-Xsuppress-version-warnings")
     }
 
     defaultConfig {
